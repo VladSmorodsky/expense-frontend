af0310a6c9f36f6b3b18484cd6eed5b67cf34289
Vlad Smorodsky <smorodsky.odesa@gmail.com>
2023-07-24 09:27:24 +0300

    EF-1 Add routing: added Dashboard, Expenses, Login and Not Found routes

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,5 @@
+export function Dashboard() {
+  return (
+    <div>Home</div>
+  );
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export function NotFound() {
+  return (
+    <div>
+      <p>Not Found</p>
+    </div>
+  );
+}
